4e73f9236552c8a670159b8c96b106b9e0c7777e
Roy Peled <roy.peled@walkme1275.local>
2021-12-07 10:53:48 +0200

    first

---
+++ index.mjs
@@ -0,0 +1,55 @@
+import express from 'express';
+import cors from 'cors';
+import validator from 'email-validator';
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(cors());
+
+const noEmailErr = {
+	type: 'NO_EMAIL',
+	message: 'email is required'
+}
+
+const invalidEmailErr = {
+	type: 'INVALID_EMAIL',
+	message: 'email is invalid'
+}
+
+const duplicateEmailErr = {
+	type: 'EXISTING_EMAIL',
+	message: 'email already exists'
+}
+
+const emails = new Set([
+	'peled@rezonate.io',
+	'amiga@rezonate.io',
+	'a@b.com',
+]);
+
+app.get('/validate', (req, res) => {
+	const { email } = req.query;
+
+	if(!email)
+		return res
+			.status(400)
+			.send(noEmailErr);
+
+	if(!validator.validate(email))
+		return res
+			.status(400)
+			.send(invalidEmailErr);
+
+	if(emails.has(email))
+		return res
+			.status(403)
+			.send(duplicateEmailErr);
+
+	res.send({ok: 1});
+});
+
+app.listen(PORT);
+console.log('listening on ' + PORT);
+
